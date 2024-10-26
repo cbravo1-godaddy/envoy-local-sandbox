fd7e0ef2e885765bbfdd9ce718965091d70a6465
Christian Bravo <cbravo1@godaddy.com>
2024-10-25 19:37:06 -0500

    Adding an example in http.yaml envoy of how to deal with custom headers based on the client request headers

--- apps/main.py
+++ apps/main.py
@@ -1,18 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 import argparse
 import logging
 import uvicorn
 
 
 def get_app(app_value):
-    #Fast API
+    # Fast API
     app = FastAPI()
     logger = logging.getLogger(__name__)
 
-
     @app.get("/")
-    def read_root():
-        return {"Hello": f"World from app {app_value}"}
+    def read_root(request: Request):
+        x_dsa_host = request.headers.get("x-dsa-host")
+
+        # Print the header value
+        print(f"x-dsa-host: {x_dsa_host}")
+
+        return {"Hello": f"World from app {app_value}, x-dsa-host: {x_dsa_host}"}
 
     @app.get("/static/homepage")
     def get_static_homepage():
@@ -22,8 +26,12 @@ def get_app(app_value):
     @app.get("/static/{static_path_asset}")
     def get_static_asset(static_path_asset):
         return {"static asset": static_path_asset}
-    
-    return app;
+
+    @app.get("/admin")
+    def get_admin():
+        return {"adminState": True}
+
+    return app
 
 
 def start_server(app_value, app_port):
@@ -32,12 +40,13 @@ def start_server(app_value, app_port):
     server = uvicorn.Server(config)
     server.run()
 
+
 # Running programmatically - https://www.uvicorn.org/#running-programmatically
 if __name__ == "__main__":
-    #CLI arg parser
-    parser = argparse.ArgumentParser(description='Init a single app')
-    parser.add_argument('--appNumber', type=int, help='App number')
-    parser.add_argument('--port', type=int, help='Server app port')
+    # CLI arg parser
+    parser = argparse.ArgumentParser(description="Init a single app")
+    parser.add_argument("--appNumber", type=int, help="App number")
+    parser.add_argument("--port", type=int, help="Server app port")
     args = parser.parse_args()
     app_value = args.appNumber
     app_port = args.port
